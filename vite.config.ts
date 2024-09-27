e14fba9cb5a771572d3c199890d8a7c38308b7df
Sarah Wang <millimillenary@gmail.com>
2024-09-27 12:29:56 +1000

    Add vite-plugin-checker
        pnpm add --save-dev vite-plugin-checker
    
        https://vite-plugin-checker.netlify.app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    checker({
+      eslint: {
+        lintCommand: "eslint .",
+        useFlatConfig: true,
+      },
+      typescript: {
+        buildMode: true,
+      },
+    }),
+    react(),
+  ],
 });
