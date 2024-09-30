622488020d6f6b02abb6515bb3d736b6d99d74eb
Sarah Wang <millimillenary@gmail.com>
2024-09-30 19:58:50 +1000

    Add PUT /api/item

--- database.ts
+++ database.ts
@@ -50,6 +50,26 @@ export class DatabaseReadOnly {
   /**
    * Watchlist table
    */
+  getItem(id: number): Promise<WatchlistDbItem | undefined> {
+    const sql = `
+      SELECT
+        id, description, url, keepMonitoring,
+        dateUpdated, lowestPrice, currentPrice, currentDiscount
+      FROM watchlist
+      WHERE id=?;
+    `;
+
+    return new Promise((resolve, reject) => {
+      this.db.get(sql, [id], (err, row: WatchlistDbItem | undefined) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+
   getItems(): Promise<WatchlistDbItem[]> {
     const sql = `
       SELECT
@@ -80,5 +100,29 @@ export class Database extends DatabaseReadOnly {
   static open(): Database {
     const db = new sqlite3.Database(DatabaseFilePath);
     return new Database(db);
+  }
+
+  /**
+   * Watchlist table
+   */
+  insertItem(description: string, url: URL): Promise<number> {
+    const sql = `
+      INSERT INTO watchlist(description, url) VALUES (?,?)
+      RETURNING id;
+    `;
+
+    return new Promise((resolve, reject) => {
+      this.db.each(
+        sql,
+        [description, url.href],
+        (err, row: WatchlistDbItem) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(row.id);
+          }
+        },
+      );
+    });
   }
 }
--- server.ts
+++ server.ts
@@ -2,8 +2,53 @@ import express, { Express, Request, Response } from "express";
 import { Database, DatabaseReadOnly } from "./database";
 
 const app: Express = express();
+app.use(express.json());
 
 Database.create();
+
+/**
+ * Insert a new item (description and url) into the watchlist.
+ * Returns the new WatchlistItem.
+ */
+interface PutItemInterface {
+  description: string;
+  url: string;
+}
+
+app.put("/api/item", async (req: Request, res: Response) => {
+  let db;
+  try {
+    const body = req.body as PutItemInterface;
+    if (
+      req.body === undefined ||
+      !("description" in req.body) ||
+      !("url" in req.body) ||
+      !URL.canParse(body.url)
+    ) {
+      // Data validation failed.
+      // return: 400 bad request.
+      res.status(400).send();
+      return;
+    }
+
+    const url = new URL(body.url);
+
+    db = Database.open();
+    const id = await db.insertItem(body.description, url);
+    const insertedItem = await db.getItem(id);
+
+    // Everything went smoothly.
+    // return 200 ok and new WatchlistItem.
+    res.status(200).send(insertedItem);
+  } catch (err: unknown) {
+    // Unknown exception.
+    // return: 500 internal server error.
+    console.error(err);
+    res.status(500).send(err);
+  } finally {
+    db?.close();
+  }
+});
 
 /**
  * Get the list of WatchlistItems from the database.
